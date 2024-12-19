30d444beb238e7ac92dad1ca40cce11fdb2eab27
Mableton <maruro2002@icloud.com>
2024-12-19 18:43:10 +0100

    Initial Commit

---
+++ Film RankingTests/Film_RankingTests.swift
@@ -0,0 +1,17 @@
+//
+//  Film_RankingTests.swift
+//  Film RankingTests
+//
+//  Created by Manoah Ruiz Rodriguez on 12/19/24.
+//
+
+import Testing
+@testable import Film_Ranking
+
+struct Film_RankingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
